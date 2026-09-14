842ab14fa32e5fb1b5afffe83ce3436417e481aa
8-ball43 <mateuszzawisza13@gmail.com>
2026-09-14 13:20:39 +0200

    Add files via upload

---
+++ silnia/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        int number = 1;
+        for(int i=1;i<=5;i++){
+            number=number * i;
+            if(i==5){
+                System.out.println("Silnia z 5 to " + number);
+            }
+        }
+       
+    }
+}
